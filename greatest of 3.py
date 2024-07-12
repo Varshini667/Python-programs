82f6117b5782c79f0a0d89da651e3b3792d6e54c
Varshini667 <varshinih667@gmail.com>
2024-07-12 15:48:45 +0530

    Create greatest of 3.py

---
+++ greatest of 3.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number"))
+b=int(input("enter b number"))
+c=int(input("enter c number"))
+if a>b and c:
+    print("a is greater")
+elif b>c and a:
+    print("b is greater")
+else:
+    print("c is greater")
